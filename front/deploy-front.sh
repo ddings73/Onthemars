a2fd414ec9bf06a165fb9a7763342b6f2b194c87
ddings <ddings7303@gmail.com>
2023-03-14 01:04:05 +0900

    Fix deploy-front.sh 수정

--- front/deploy-front.sh
+++ front/deploy-front.sh
@@ -1,5 +1,6 @@
 sudo docker ps -a -q --filter "name=front" | grep -q . && docker stop front && docker rm front | true
 sudo docker rmi e207/front:1.0
 sudo docker pull e207/front:1.0
-docker run -d -p 80:80 --name front e207/front:1.0
+
+docker run -d -p 80:80 -p 443:443 -v /home/ubuntu/certbot/conf:/etc/letsencrypt/ -v /home/ubuntu/certbot/www:/var/www/certbot --name front e207/front:1.0
 docker image prune -af
