e802cb78f6778120c38d5b165b0cf8b5c723f6c6
ddings <ddings7303@gmail.com>
2023-03-14 21:48:31 +0900

    Fix deploy-back.sh 이미지 분리

--- back/scripts/deploy-back.sh
+++ back/scripts/deploy-back.sh
@@ -7,20 +7,25 @@ if [ $CURRENT_PROFILE == dev1 ]
 then
         IDLE_PROFILE=dev2
         IDLE_PORT=8082
+				IDLE_TAG=2.0
+				CURRENT_TAG=1.0
 elif [ $CURRENT_PROFILE == dev2 ]
 then
         IDLE_PROFILE=dev1
         IDLE_PORT=8081
+				IDLE_TAG=1.0
+				CURRENT_TAG=2.0
 else
         echo "> 일치하는 Profile이 없습니다. Profile: $CURRENT_PROFILE"
-        echo "> dev1을 할당합니다. IDLE_PROFILE: dev1"
         IDLE_PROFILE=dev1
         IDLE_PORT=8081
+				IDLE_TAG=1.0
+				CURRENT_TAG=2.0
 fi
 
-sudo docker ps -a -q --filter "name=${IDLE_PROFILE}" | grep -q . && docker stop $IDLE_PROFILE && docker rm $IDLE_PROFILE | true
-sudo docker pull e207/back:1.0
-docker run -d -p $IDLE_PORT:${IDLE_PORT} --name $IDLE_PROFILE -e Profile=$IDLE_PROFILE e207/back:1.0
-docker image prune -af
+sudo docker ps -a -q --filter "name=back-${CURRENT_PROFILE}" | grep -q . && docker stop back-$CURRENT_PROFILE && docker rm back-$CURRENT_PROFILE | true
+sudo docker rmi e207/back:$CURRENT_TAG
+sudo docker pull e207/back:$IDLE_TAG
+docker run -d -p $IDLE_PORT:${IDLE_PORT} --name back-$IDLE_PROFILE -e Profile=$IDLE_PROFILE e207/back:$IDLE_TAG
 
 sh switch.sh
