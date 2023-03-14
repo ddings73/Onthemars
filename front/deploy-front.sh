f86b5e301b3cd7cd278f7490ae9d3825b55bb972
ddings <ddings7303@gmail.com>
2023-03-14 17:07:40 +0900

    Fix 배포파일 수정

--- front/deploy-front.sh
+++ front/deploy-front.sh
@@ -2,5 +2,5 @@ sudo docker ps -a -q --filter "name=front" | grep -q . && docker stop front && d
 sudo docker rmi e207/front:1.0
 sudo docker pull e207/front:1.0
 
-docker run -d -p 80:80 -p 443:443 -v /home/ubuntu/dev/service-url.inc:/etc/nginx/conf.d/service-url.inc -v /home/ubuntu/certbot/conf:/etc/letsencrypt/ -v /home/ubuntu/certbot/www:/var/www/certbot --name front e207/front:1.0
+docker run -d -p 80:80 -p 443:443 -v /home/ubuntu/dev/conf.d:/etc/nginx/conf.d/ -v /home/ubuntu/certbot/conf:/etc/letsencrypt/ -v /home/ubuntu/certbot/www:/var/www/certbot --name front e207/front:1.0
 docker image prune -af
