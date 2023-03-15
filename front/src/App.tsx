a28df22e9e7696371f18e6f9da472c52bfe1b471
김승섭 <sub9707@naver.com>
2023-03-15 17:47:33 +0900

    feat 404 페이지

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,6 +2,8 @@ import GlobalFont from './styles/GlobalFont';
 import Router from './routes';
 import NavBar from 'component/navbar/NavBar';
 import styles from './App.module.scss';
+import * as React from 'react';
+import scrollbar from 'smooth-scrollbar';
 
 function App() {
   return (
