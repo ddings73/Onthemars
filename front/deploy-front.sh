bbcd95c630a85963d9d5ac8b0ad2b52ede681938
ddings <ddings7303@gmail.com>
2023-03-16 14:15:56 +0900

    Fix blue-green 배포 적용

--- front/deploy-front.sh
+++ front/deploy-front.sh
@@ -1,5 +1,32 @@
-sudo docker ps -a -q --filter "name=front" | grep -q . && docker stop front && docker rm front | true
-sudo docker rmi e207.front:1.0
-sudo docker pull e207/front:1.0
-docker run -d -p 80:80 -p 443:443 -v /home/ubuntu/dev/conf.d:/etc/nginx/conf.d -v /home/ubuntu/certbot/conf:/etc/letsencrypt/ -v /home/ubuntu/certbot/www:/var/www/certbot --name front e207/front:1.0
+echo "> 현재 구동중인 profile 확인"
+CURRENT_PROFILE=$(curl -X POST https://j8e207.p.ssafy.io/api/v1/profile)
+
+echo "> $CURRENT_PROFILE"
+
+if [ $CURRENT_PROFILE == v1 ]
+then
+        IDLE_PROFILE=v2
+        IDLE_TAG=2.0
+        IDLE_PORT=3001
+        CURRENT_TAG=1.0
+elif [ $CURRENT_PROFILE == v2 ]
+then
+        IDLE_PROFILE=v1
+        IDLE_TAG=1.0
+        IDLE_PORT=3002
+        CURRENT_TAG=2.0
+else
+        echo "> 일치하는 Profile이 없습니다. Profile: $CURRENT_PROFILE"
+        IDLE_PROFILE=v1
+        IDLE_TAG=1.0
+        IDLE_PORT=3001
+        CURRENT_TAG=2.0
+fi
+
+
+sudo docker ps -a -q --filter "name=front-${IDLE_PROFILE}" | grep -q . && docker stop front-$IDLE_PROFILE && docker rm front-$IDLE_PROFILE | true
+sudo docker rmi e207/front-$IDLE_PROFILE:$IDLE_TAG
+sudo docker pull e207/front-$IDLE_PROFILE:$IDLE_TAG
+docker run -d -p $IDLE_PORT:${$IDLE_PORT} --name front-$IDLE_PROFILE e207/front-$IDLE_PROFILE:$IDLE_TAG
+docker cp front-$IDLE_PROFILE:/usr/src/app/build /home/ubuntu/dev/html
 docker image prune -af
