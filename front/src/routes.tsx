dbd872a6a3a43a05257e9ac79bbc423cf2e568e4
이미현 <algus7531@naver.com>
2023-03-17 17:01:55 +0900

    Feat NFT 메인 페이지 css

--- front/src/routes.tsx
+++ front/src/routes.tsx
@@ -2,14 +2,12 @@ import TeamPage from 'Containers/ETC/TeamPage';
 import { useRoutes } from 'react-router-dom';
 
 import Home from './Containers/Main/Main';
-import NFTBodyContainer from './Containers/nftMain/NFTBodyContainer';
+import NFTBodyContainer from './Containers/NFT/NFTBodyContainer';
 import Login from 'Containers/login/Login';
 import GameMain from 'Containers/gameMain';
-import Main from 'Containers/Main/Main';
 import ErrorPage from 'Containers/Error/ErrorPage';
 
 export default function Router() {
-  const user = true;
   return useRoutes([
     {
       path: '/',
@@ -17,8 +15,11 @@ export default function Router() {
       children: [{ path: '/', element: <Home /> }],
     },
     {
-      path: '/nftMain',
+      path: '/nft',
       element: <NFTBodyContainer />,
+      children: [
+        { path: "", element: <NFTBodyContainer /> },
+      ],
     },
     {
       path: '/team',
