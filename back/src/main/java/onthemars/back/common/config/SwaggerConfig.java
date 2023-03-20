bd5f3c9708f366173c5393fb3dd58b2e78d4ed39
ddings <ddings7303@gmail.com>
2023-03-20 11:03:46 +0900

    Feature Swagger 3.0.0 추가

---
+++ back/src/main/java/onthemars/back/common/config/SwaggerConfig.java
@@ -0,0 +1,63 @@
+package onthemars.back.common.config;
+
+import com.fasterxml.classmate.TypeResolver;
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.AlternateTypeRules;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api(TypeResolver typeResolver) {
+        return new Docket(DocumentationType.OAS_30)
+            .alternateTypeRules(AlternateTypeRules.newRule(typeResolver.resolve(Pageable.class),
+                typeResolver.resolve(MyPageable.class)))
+            .useDefaultResponseMessages(true)
+            .apiInfo(apiInfo())
+            .securityContexts(Arrays.asList(securityContext()))
+            .securitySchemes(Arrays.asList(apiKey()))
+            .select()
+            .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+            .paths(PathSelectors.any())
+            .build();
+    }
+
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+            .title("Onthemars Swaggers")
+            .description("Onthemars 스웨거입니다")
+            .version("1.0")
+            .build();
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global",
+            "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+    }
+}
