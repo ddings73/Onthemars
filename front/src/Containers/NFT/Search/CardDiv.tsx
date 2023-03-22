292a024cf909b53ae601be793fd9eefc9feffb4e
이미현 <algus7531@naver.com>
2023-03-23 08:56:49 +0900

    Feat NFT 필터 page

--- front/src/Containers/NFT/Search/CardDiv.tsx
+++ front/src/Containers/NFT/Search/CardDiv.tsx
@@ -1,10 +1,21 @@
 import styles from './CardDiv.module.scss';
+import Card from 'component/nftCard/card';
 
 export function NFTCard() {
 
+  const testList = [1, 2, 3, 4]
+  const img_address = 'https://f1.tokenpost.kr/2021/12/p9f2wvlf7b.jpg'
+
   return (
     <div className={styles.container}>
-      카드
+      <div className={styles.title}>items</div>
+      <div className={styles.cardsDiv}>
+        {testList.map((test) =>
+          <div key={test} className={styles.cardDiv} >
+            <Card key={test} size='big' img_address={img_address} />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
--- front/src/Containers/NFT/Search/Filter.tsx
+++ front/src/Containers/NFT/Search/Filter.tsx
@@ -1,5 +1,5 @@
 import styles from './Filter.module.scss';
-import { Checkbox } from 'antd';
+import { Button, Checkbox, Input } from 'antd';
 import type { CheckboxValueType } from 'antd/es/checkbox/Group';
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -32,6 +32,7 @@ export function NFTFilter() {
   const mouth = ['Default', 'Smile', 'Yammy', 'Tongue', 'Sad', 'Wow', 'Chu']
   const head = ['Default', 'Hairband', 'Ribbon', 'Headset', 'Nutrients', 'Fork', 'Worm']
 
+  const [visiblePrice, setVisiblePrice] = useState(false);
   const [visibleTier, setVisibleTier] = useState(true);
   const [visibleCategory, setVisibleCategory] = useState(true);
   const [visibleBackground, setVisibleBackground] = useState(true);
@@ -42,6 +43,24 @@ export function NFTFilter() {
 
   return (
     <div className={styles.container}>
+      <div className={styles.Title} onClick={() => setVisiblePrice((prev) => !prev)} >
+        <div>Price</div>
+        <div>{
+          visiblePrice ?
+            <FontAwesomeIcon icon={faSortDown} /> :
+            <FontAwesomeIcon icon={faCaretUp} />}
+        </div>
+      </div>
+      {visiblePrice ? <></> :
+        <>
+          <div className={styles.priceDiv}>
+            <Input placeholder="Min" className={styles.inputDiv} type="text" />
+            to
+            <Input placeholder="Max" className={styles.inputDiv} type="text" />
+          </div>
+          <Button className={styles.submitButton}>BTN</Button>
+        </>
+      }
       <div className={styles.Title} onClick={() => setVisibleTier((prev) => !prev)} >
         <div>Tier</div>
         <div>{
