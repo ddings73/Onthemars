8a04dfb1c0b6137ad4bad54d1480b029b77716b3
subin <marybin99@naver.com>
2023-03-29 21:55:49 +0900

    Feature 무한스크롤

--- front/src/Containers/MyPage/Collected.tsx
+++ front/src/Containers/MyPage/Collected.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import styles from './CardList.module.scss';
 import Card from 'component/nftCard/card';
 import { Link, useParams } from 'react-router-dom';
+import { useInView } from 'react-intersection-observer';
 import axios from 'axios';
 
 function Collected() {
@@ -10,27 +11,56 @@ function Collected() {
   const [collectedList, setCollectedList] = useState([]);
   const address = useParams().address;
 
-  const getData = async () => {
+  const [loading, setLoading] = useState(false);
+  const [ref, inView] = useInView();
+  const [end, setEnd] = useState<boolean>(false);
+  const [page, setPage] = useState(0);
+  const size = 4;
+
+  const getData = useCallback(async () => {
     await axios({
       method: 'get',
-      url: baseURL + `/nft/${address}/collected`,
+      url: baseURL + `/nft/${address}/collected?page=${page}&size=${size}`,
     }).then((res) => {
-      setCollectedList(res.data);
+      if (res.data.length !== 0) {
+        setCollectedList((prevState): any => [...prevState, ...res.data]);
+      } else {
+        setEnd(true);
+      }
     });
-  };
-  
+    setLoading(false);
+  }, [page, address]);
+
   useEffect(() => {
-    getData();
-  }, []);
+    if (!end) {
+      getData();
+    }
+  }, [getData, end]);
+
+  useEffect(() => {
+    if (inView && !loading) {
+      setPage((prevState) => prevState + 1);
+    }
+  }, [inView, loading]);
 
   return (
     <div className={styles.container}>
-      {collectedList.map((item:any, index:number) => (
-        <div key={index} className={styles.cardList}>
-          <Link to={`${item}`}>
-            <Card size="bigg" img_address={imgBaseURL+item.imgUrl}></Card>
-          </Link>
-        </div>
+      {collectedList.map((item: any, index: number) => (
+        <React.Fragment key={index}>
+          {collectedList.length - 1 === index ? (
+            <div key={index} className={styles.cardList} ref={ref}>
+              <Link to={`${item}`}>
+                <Card size="bigg" img_address={imgBaseURL + item.imgUrl}></Card>
+              </Link>
+            </div>
+          ) : (
+            <div key={index} className={styles.cardList}>
+              <Link to={`${item}`}>
+                <Card size="bigg" img_address={imgBaseURL + item.imgUrl}></Card>
+              </Link>
+            </div>
+          )}
+        </React.Fragment>
       ))}
     </div>
   );
--- front/src/Containers/MyPage/Minted.tsx
+++ front/src/Containers/MyPage/Minted.tsx
@@ -1,36 +1,68 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import styles from './CardList.module.scss';
 import Card from 'component/nftCard/card';
 import { Link, useParams } from 'react-router-dom';
+import { useInView } from 'react-intersection-observer';
 import axios from 'axios';
 
 function Minted() {
   const baseURL = 'https://j8e207.p.ssafy.io/api/v1';
   const imgBaseURL = 'https://onthemars-dev.s3.ap-northeast-2.amazonaws.com/';
+
   const [mintedList, setMintedList] = useState([]);
   const address = useParams().address;
 
-  const getData = async () => {
+  const [loading, setLoading] = useState(false);
+  const [ref, inView] = useInView();
+  const [end, setEnd] = useState<boolean>(false);
+  const [page, setPage] = useState(0);
+  const size = 4;
+
+  const getData = useCallback(async () => {
+    setLoading(true);
     await axios({
       method: 'get',
-      url: baseURL + `/nft/${address}/minted`,
+      url: baseURL + `/nft/${address}/minted?page=${page}&size=${size}`,
     }).then((res) => {
-      setMintedList(res.data);
+      if (res.data.length !== 0) {
+        setMintedList((prevState): any => [...prevState, ...res.data]);
+      } else {
+        setEnd(true);
+      }
     });
-  };
+    setLoading(false);
+  }, [page, address]);
 
   useEffect(() => {
-    getData();
-  }, []);
+    if (!end) {
+      getData();
+    }
+  }, [getData, end]);
+  
+  useEffect(() => {
+    if (inView && !loading) {
+      setPage((prevState) => prevState + 1);
+    }
+  }, [inView, loading]);
 
   return (
     <div className={styles.container}>
       {mintedList.map((item: any, index: number) => (
-        <div key={index} className={styles.cardList}>
-          <Link to={`${item}`}>
-            <Card size="bigg" img_address={imgBaseURL + item.imgUrl}></Card>
-          </Link>
-        </div>
+        <React.Fragment key={index}>
+        {mintedList.length - 1 === index ? (
+          <div key={index} className={styles.cardList} ref={ref}>
+            <Link to={`${item}`}>
+              <Card size="bigg" img_address={imgBaseURL + item.imgUrl}></Card>
+            </Link>
+          </div>
+        ) : (
+          <div key={index} className={styles.cardList}>
+            <Link to={`${item}`}>
+              <Card size="bigg" img_address={imgBaseURL + item.imgUrl}></Card>
+            </Link>
+          </div>
+        )}
+      </React.Fragment>
       ))}
     </div>
   );
