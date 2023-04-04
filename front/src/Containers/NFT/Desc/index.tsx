13be38603978d61f2129cbac111f5a8a9baf259f
황수빈 <marybin99@naver.com>
2023-04-04 10:39:19 +0900

    Feature nftdesc

--- front/src/Containers/NFT/Desc/index.tsx
+++ front/src/Containers/NFT/Desc/index.tsx
@@ -1,7 +1,14 @@
 import styles from './index.module.scss';
+import nftdesc from 'assets/nftdesc.png';
 
 function NFTDesc() {
-  return <div className={styles.container}>설명띠</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.desc}>
+        <img src={nftdesc} alt="" />
+      </div>
+    </div>
+  );
 }
 
 export default NFTDesc;
